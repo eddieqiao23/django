75a0d13c4950e18c94164eaea0c4f01cc135f429
Eddie Qiao <eddie.qiao.23@bishops.com>
2021-04-04 15:09:45 -0700

    set up log in system but it breaks a lot

--- quiz/views.py
+++ quiz/views.py
@@ -1,16 +1,43 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Question, Quiz
+
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+
 
 def index(request):
     quizzes = Quiz.objects.all()
 
+    failed = False
+
+    if request.POST:
+        if 'inputUsername' in request.POST.keys():
+            user = authenticate(username=request.POST['inputUsername'], password=request.POST['inputPassword'])
+            if user is not None:
+                login(request, user)
+            else:
+                failed = True
+                # failed login
+        elif 'logout' in request.POST.keys():
+            logout(request)
+    if request.user.is_authenticated:
+        loggedIn = True
+    else:
+        loggedIn = False
+
+    template = loader.get_template('quiz/index.html')
+
     context = {
         'quizzes': quizzes,
+        'loggedIn': loggedIn,
+        'user': request.user,
+        'failed': failed
     }
-    # return HttpResponse(output)
-    return render(request, 'quiz/index.html', context)
+
+    return HttpResponse(template.render(context, request))
 
 def quiz_details(request, quiz_id):
     quiz = get_object_or_404(Quiz, pk = quiz_id)
